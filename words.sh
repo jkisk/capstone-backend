73545c6148669fe65e521827622c413f6d702261
Jamie Kinkead <jkinkead@gmail.com>
2019-02-13 12:10:23 -0800

    game scripts added

--- words.sh
+++ words.sh
@@ -1,3 +1,52 @@
 
 
-curl http://localhost:8080/games -d := {"string":"acrobat"}
+
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"string":"academy"}' \
+  http://localhost:8080/games
+
+  curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"string":"acrobat"}' \
+  http://localhost:8080/games
+
+  curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"string":"address"}' \
+  http://localhost:8080/games
+
+  curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"string":"advisor"}' \
+  http://localhost:8080/games
+
+  curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"string":"abandon"}' \
+  http://localhost:8080/games
+
+  curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"string":"answers"}' \
+  http://localhost:8080/games
+
+  curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"string":"beneath"}' \
+  http://localhost:8080/games
+
+  curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"string":"benefit"}' \
+  http://localhost:8080/games
+
+  curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"string":"builder"}' \
+  http://localhost:8080/games
+
+  curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"string":"bravery"}' \
+  http://localhost:8080/games
