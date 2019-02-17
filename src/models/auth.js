bdfbe70e1ddea542bc5d7f6d741856c387d615a7
Jamie Kinkead <jkinkead@gmail.com>
2019-02-16 18:55:59 -0800

    fixed score id

--- src/models/auth.js
+++ src/models/auth.js
@@ -15,6 +15,7 @@ const login = (playername, password) => {
     .then(status => {
       if (!status) throw { status: 401, message: 'Unauthorized' }
       delete player.password
+      console.log(player)
       return player
     })
 }
